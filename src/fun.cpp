809c72bc5f39a91381da11b55093cbed92c620e0
Kingalldog <114134222+Kingalldog@users.noreply.github.com>
2025-03-08 13:50:07 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,5 +2,13 @@
 #include "fun.h"
 
 int64_t power(int64_t x, uint16_t n) {
-    return 0;
+  int64_t ans = 1;
+  while(n > 0){
+    if(n % 2){
+        ans *= x;
+    }
+    n /=2;
+    x *= x;
+  }
+  return ans;
 }
